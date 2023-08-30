458424df6674c1a7f5f9f61ed572b3a7faa0fd32
leehomeecky <meecky4chist@gmail.com>
2023-08-30 22:55:27 +0100

    project set-up

---
+++ src/main/java/com/meecky/SCRAT/ScratApplication.java
@@ -0,0 +1,13 @@
+package com.meecky.SCRAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScratApplication.class, args);
+	}
+
+}
